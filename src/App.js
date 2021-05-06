25a66dd8a64906a17ff93f07fd2269d961c749bf
GoldCaesar <80002617+GoldenCaesar@users.noreply.github.com>
2021-05-06 11:27:21 -0700

    ttt

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ function App() {
           Learn React
         </a>
       </header> */}
-<div class="overlay"></div>
-  <div class="scanline"></div>
-  <section class="screen">
-  <div class="" id="loading"  {/*style="overflow-y: auto;"*/}></div> 
-  <div class="hide" id="allContent">
-  <div class="wrapper">	
-    <div class="content clearfix">
-      <header class="site clearfix">
-      <div class="col one">
+<div className="overlay"></div>
+  <div className="scanline"></div>
+  <section className="screen">
+  <div className="" id="loading"  {/*style="overflow-y: auto;"*/}></div> 
+  <div className="hide" id="allContent">
+  <div className="wrapper">	
+    <div className="content clearfix">
+      <header className="site clearfix">
+      <div className="col one">
           <p>Demicube, INC</p>
       <img src="assets/demicube.png" width="740" height="729" id="logo-v" />
       </div>
-          <div class="col two">
+          <div className="col two">
               <h4>Demicube, INC (tm) <br /> <b>H</b>ash <b>E</b>ncrypted <b>R</b>eal-Time <b>O</b>perating <b>S</b>ystem (HEROS)</h4>
               <p>----------------------------------------</p>
               <p>HEROS v 1.0.0</p>
@@ -41,16 +41,16 @@ function App() {
             <p>Cascading Styler Inspired by M. Richardson</p>
           </div>
       </header>
-      <p class="clear"><br /></p>
+      <p className="clear"><br /></p>
       <p>Welcome to the Hash Encrypted Real-Time Operating System (HEROS). <br/>Fill out the fields below to Encrypt or Decrypt your data. There is no way to recover encrypted data without the password, and there is no password recovery. Version or algorithm change may render encrypted data lost. Goodluck.</p><br />
     
-      <div class="goodLuck">
-        <div class="" id="nCode">
+      <div className="goodLuck">
+        <div className="" id="nCode">
         <h1>Encode or Decode your string</h1>
-        <label class="encodeLabel">String to Encode</label>
-        <textarea class="textEncodeInput" id="str" type="text"  {/*style="border-bottom: solid; margin-bottom: 1em;"*/}></textarea> 
-        <label class="passwordLabel">Password</label>
-         <input class="passwordEncodeInput" id="pass" type="text" {/*style="border-bottom: solid; margin-bottom: 1em;"*/}/> 
+        <label className="encodeLabel">String to Encode</label>
+        <textarea className="textEncodeInput" id="str" type="text"  {/*style="border-bottom: solid; margin-bottom: 1em;"*/}></textarea> 
+        <label className="passwordLabel">Password</label>
+         <input className="passwordEncodeInput" id="pass" type="text" {/*style="border-bottom: solid; margin-bottom: 1em;"*/}/> 
         <button id="encodeButton">Encode</button>
         <button id="decodeButton">Decode</button>
         <br/>
@@ -58,7 +58,7 @@ function App() {
         <button id="clearBtn">Clear</button>
         <h3>Result:</h3>
         <br/>
-        <textarea class="result" id=""></textarea>
+        <textarea className="result" id=""></textarea>
         </div>
       </div>
 
